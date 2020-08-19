93fc56cf032d26db27eca88d777e44d923b53cb5
raju-ahammad <web.rajuahammad@gmail.com>
2020-08-20 02:32:40 +0600

    create testimonial app initial

--- code/js/slider.js
+++ code/js/slider.js
@@ -12,7 +12,7 @@ const leftButton = document.querySelector(".l-btn")
 const rightButton = document.querySelector(".r-btn")
 
 imgContainer.style.backgroundImage = `url('../img/london.jpg')`
-imgContainer.style.width = "100%"
+
 let counter = 0;
 
 
@@ -32,36 +32,3 @@ rightButton.onclick = function () {
     imgContainer.style.backgroundImage = `url('../img/${pictures[counter]}')`
 }
 
-
-// (function() {
-//     const pictures = [
-//         "bridge.jpg",
-//         "field.jpg",
-//         "taj.jpg",
-//         "tour_1.jpg"
-//     ];
-  
-//     //select both left and right button. Add event listeners
-//     const buttons = document.querySelectorAll('.btn')
-//     const imageDIV = document.querySelector('.img-container')
-//     let counter = 0
-  
-//     buttons.forEach(function(button){
-//       button.addEventListener('click', function(e){
-//         if (button.classList.contains('btn-left')){
-//           counter--
-//           if(counter < 0){
-//             counter = pictures.length -1
-//           }
-//           imageDIV.style.backgroundImage = `url('../img/${pictures[counter]}')`
-//         }
-//         if (button.classList.contains('btn-right')){
-//           counter++
-//           if(counter > pictures.length -1){
-//             counter = 0
-//           }
-//           imageDIV.style.backgroundImage = `url('../img/${pictures[counter]}')`
-//         }
-//       })
-//     })
-//   })();
