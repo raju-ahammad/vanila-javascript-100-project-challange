5c0cb65d2082ec022054297104ff715652390232
raju-ahammad <web.rajuahammad@gmail.com>
2020-09-07 16:30:33 +0600

    finshed nav toggle peojecrt

---
+++ code/js/navabr.js
@@ -0,0 +1,15 @@
+const navToggle = document.querySelector(".nav-toggle")
+const links = document.querySelector(".links")
+nav = document.querySelector(".nav")
+navToggle.addEventListener("click", function() {
+    if (links.classList.contains("show")) {
+        links.classList.remove("show")
+        nav.style.height = "auto"
+    }
+    else {
+        links.classList.add("show")
+        nav.style.height = "100px"
+
+
+    }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   purge: [],
   theme: {
+    screens: {
+      'xs': '400px',
+      'sm': '600px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'xxl': '1590px',
+    },
     extend: {},
   },
   variants: {},
